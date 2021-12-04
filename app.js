14c4a5bbd4bf9412294c3639e63bd0f36a7344c5
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2021-12-03 21:13:23 -0500

    began scoring for pure types

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const getTypeData = require('./src/get-type-data');
 
+let typeDamageRelationMap = new Map();
+
 // getting names
-const run = async () => {
+const createDamageSets = async () => {
   for (let i = 1; i <= 18; i++) {
-    const type = await getTypeData(i);
-    console.log(type.name);
-    console.log(type.damage_relations)
+    const typeData = await getTypeData(i);
+    typeDamageRelationMap.set(typeData.name, typeData.damage_relations);
   }
+
+  typeDamageRelationMap.forEach((typeRelation, typeName) => {
+    // console.log(typeName, typeRelation);
+    let score = 0;
+    let typeCount = 0;
+    score -= typeRelation.double_damage_from.length * 2;
+    typeCount += typeRelation.double_damage_from.length;
+    score += typeRelation.double_damage_to.length * 2;
+    typeCount += typeRelation.double_damage_to.length;
+    score += typeRelation.half_damage_from.length * 2;
+    typeCount += typeRelation.half_damage_from.length;
+    score -= typeRelation.half_damage_to.length * 2;
+    typeCount += typeRelation.half_damage_to.length;
+    score += typeRelation.no_damage_from.length * 4;
+    typeCount += typeRelation.no_damage_from.length;
+    score -= typeRelation.no_damage_to.length * 4;
+    typeCount += typeRelation.no_damage_to.length;
+
+    score += 18 - typeCount;
+
+    console.log(typeName, score);
+  });
 };
 
-run();
+createDamageSets();
