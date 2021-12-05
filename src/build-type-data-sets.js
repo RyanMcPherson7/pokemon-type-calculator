055fd5772b5ac1fd4b05bb90b6b561c60ed57421
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2021-12-04 19:52:36 -0500

    added build type data set file

---
+++ src/build-type-data-sets.js
@@ -0,0 +1,29 @@
+const { getTypeData } = require('./get-type-data');
+
+exports.buildTypeDataSets = async (typeDmgMap, typeCombos) => {
+  let types = [];
+
+  // building type name list and type damage relation map
+  for (let i = 1; i <= 18; i++) {
+    const typeData = await getTypeData(i);
+    types.push(typeData.name);
+    typeDmgMap.set(typeData.name, typeData.damage_relations);
+  }
+
+  // building type combination list
+  for (let i = 0; i < 18; i++) {
+    // pure types
+    typeCombos.push([types[i]]);
+
+    // duel types
+    for (let j = i + 1; j < 18; j++) {
+      typeCombos.push([types[i], types[j]]);
+    }
+  }
+
+  // testing
+  typeCombos.forEach((item) => {
+    console.log(item);
+  });
+  console.log(typeCombos.length);
+};
