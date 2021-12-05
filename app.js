087907296256c6ed8d0dc2f83bfa5cb6b30b0d79
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2021-12-04 19:44:45 -0500

    added type combo builder

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 const { getTypeData } = require('./src/get-type-data');
 
-let typeDamageRelationMap = new Map();
-
 // getting names
-const createDamageSets = async () => {
+const buildTypeDataSets = async (typeList, typeDamageRelationMap) => {
   for (let i = 1; i <= 18; i++) {
     const typeData = await getTypeData(i);
+    typeList.push(typeData.name);
     typeDamageRelationMap.set(typeData.name, typeData.damage_relations);
   }
-
-  typeDamageRelationMap.forEach((typeRelation, typeName) => {
-    let score = 0;
-    const damageMult = 1;
-    score -= typeRelation.double_damage_from.length * damageMult;
-    score += typeRelation.double_damage_to.length * damageMult;
-    score += typeRelation.half_damage_from.length * damageMult;
-    score -= typeRelation.half_damage_to.length * damageMult;
-    score += typeRelation.no_damage_from.length * damageMult * 2;
-    score -= typeRelation.no_damage_to.length * damageMult * 2;
-
-    console.log(typeName, score);
-  });
 };
 
-createDamageSets();
+const buildTypeCombinations = async (typeList, typeDamageRelationMap, typeCombinations) => {
+  await buildTypeDataSets(typeList, typeDamageRelationMap);
+
+  for (let i = 0; i < 18; i++) {
+    // pure types
+    typeCombinations.push([typeList[i]]);
+
+    // duel types
+    for (let j = i + 1; j < 18; j++) {
+      typeCombinations.push([typeList[i], typeList[j]]);
+    }
+  }
+
+  typeCombinations.forEach((item) => {
+    console.log(item);
+  });
+  console.log(typeCombinations.length);
+};
+
+
+let types = [];
+let dmgMap = new Map();
+let combos = [];
+buildTypeCombinations(types, dmgMap, combos);
