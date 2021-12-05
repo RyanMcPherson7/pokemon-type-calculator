bb24c66bb6ea243cd9d52defaf80d64737552925
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2021-12-04 19:52:15 -0500

    modualted build type data set

--- app.js
+++ app.js
@@ -1,35 +1,5 @@
-const { getTypeData } = require('./src/get-type-data');
+const {buildTypeDataSets} = require('./src/build-type-data-sets')
 
-// getting names
-const buildTypeDataSets = async (typeList, typeDamageRelationMap) => {
-  for (let i = 1; i <= 18; i++) {
-    const typeData = await getTypeData(i);
-    typeList.push(typeData.name);
-    typeDamageRelationMap.set(typeData.name, typeData.damage_relations);
-  }
-};
-
-const buildTypeCombinations = async (typeList, typeDamageRelationMap, typeCombinations) => {
-  await buildTypeDataSets(typeList, typeDamageRelationMap);
-
-  for (let i = 0; i < 18; i++) {
-    // pure types
-    typeCombinations.push([typeList[i]]);
-
-    // duel types
-    for (let j = i + 1; j < 18; j++) {
-      typeCombinations.push([typeList[i], typeList[j]]);
-    }
-  }
-
-  typeCombinations.forEach((item) => {
-    console.log(item);
-  });
-  console.log(typeCombinations.length);
-};
-
-
-let types = [];
 let dmgMap = new Map();
 let combos = [];
-buildTypeCombinations(types, dmgMap, combos);
+buildTypeDataSets(dmgMap, combos);
--- src/get-type-data.js
+++ src/get-type-data.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-// import axios from 'axios';
 
 exports.getTypeData = async (type) => {
   const res = await axios({
